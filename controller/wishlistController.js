86ff90f25023f2baf693042f0656182b976535f3
Hammad Afzal <hammad.afzal.code@gmail.com>
2024-08-02 10:32:09 +0500

    fix repetative email issue

--- controller/wishlistController.js
+++ controller/wishlistController.js
@@ -4,8 +4,16 @@ const addEmail = async (req, res) => {
   const { email } = req.body;
 
   try {
-    const newEmail = await Wishlist.create({ email });
-    res.status(201).json(newEmail);
+    const existingEmail = await Wishlist.findOne({ where: { email } });
+
+    if (existingEmail) {
+      return res
+        .status(409)
+        .json({ message: "Email already exists in our database" });
+    } else {
+      const newEmail = await Wishlist.create({ email });
+      res.status(201).json(newEmail);
+    }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
